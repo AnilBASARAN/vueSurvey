f9f72dc8253236a950550bacfbfa4ae94d56b5a3
AnilBASARAN <nlbsrn@gmail.com>
2025-06-01 22:01:14 +0300

    handle the no data state

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -8,7 +8,8 @@
         >
       </div>
       <p v-if="isLoading">Loading</p>
-      <ul v-else>
+
+      <ul v-else-if="!isLoading && results && results.length > 0">
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -16,6 +17,7 @@
           :rating="result.rating"
         ></survey-result>
       </ul>
+      <p v-else>No stored experiences found.</p>
     </base-card>
   </section>
 </template>
